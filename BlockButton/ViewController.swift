36286b31e4f832793a19ccf599e627a9e4c14cb8
Tim Bolstad <tim@skilpaddelabs.com>
2016-04-14 09:46:15 -0400

    Added Change button.

--- BlockButton/ViewController.swift
+++ BlockButton/ViewController.swift
@@ -9,26 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var changeIndex = 0
+    
     @IBOutlet weak var blockView: SPLBlockView!
     @IBOutlet weak var blockButton: SPLBlockButton!
     @IBOutlet weak var disableOrangeButton: SPLBlockButton!
+    @IBOutlet weak var changeButton: SPLBlockButton!
     
     @IBAction func orangeButtonClick(sender: SPLBlockButton) {
         print("Orange")
     }
     
     @IBAction func disableOrangeClicked(sender: SPLBlockButton) {
+        
         blockButton.enabled = !blockButton.enabled
         let newTitle = blockButton.enabled ? "Disable" : "Enable"
         disableOrangeButton.setTitle( newTitle, forState: .Normal)
     }
     
+    @IBAction func changeButtonClicked(sender: SPLBlockButton) {
+
+        changeButton.tintColor = UIColor.blueColor()
+//        if changeIndex == 0 {
+//            changeButton.setBlockForState( .Normal,
+//                                drawBlock: SPLBlockView.drawTest())
+//            
+//         } else {
+//            
+//            changeButton.setBlockForState( .Normal,
+//                                drawBlock: SPLBlockView.drawRoundedRect())
+//        }
+//        changeIndex = (changeIndex + 1) % 2
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Remove colors set in IB for clarity.
-        blockView.backgroundColor = UIColor.clearColor()
-        blockButton.backgroundColor = UIColor.clearColor()
+        let clearColor = UIColor.clearColor()
+        blockView.backgroundColor = clearColor
+        blockButton.backgroundColor = clearColor
+        changeButton.backgroundColor = clearColor
         self.view.tintColor = UIColor.redColor()
     }
 
